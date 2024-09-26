517921d12e68be0b6c364ec7a5bd01bbace81e67
amiddio <amiddio.dev@gmail.com>
2024-09-26 11:46:20 +0400

    Deleted a column slug from release_types table

--- src/app/Http/Controllers/Api/V1/ReleaseTypeController.php
+++ src/app/Http/Controllers/Api/V1/ReleaseTypeController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Data\ReleaseTypeData;
 use App\Http\Controllers\Controller;
-use App\Repositories\ReleaseTypeRepository;
-use Illuminate\Database\Eloquent\Collection;
 use App\Http\Resources\ReleaseTypeResource;
+use App\Repositories\ReleaseTypeRepository;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ReleaseTypeController extends Controller
 {
 
     /**
      * @param ReleaseTypeRepository $releaseTypeRepository
+     * @return AnonymousResourceCollection
      */
-    public function __invoke(ReleaseTypeRepository $releaseTypeRepository)
+    public function __invoke(ReleaseTypeRepository $releaseTypeRepository): AnonymousResourceCollection
     {
-        //return $releaseTypeRepository->getAll();
         return ReleaseTypeResource::collection($releaseTypeRepository->getAll());
     }
 
--- src/database/seeders/ReleaseTypeSeeder.php
+++ src/database/seeders/ReleaseTypeSeeder.php
@@ -13,11 +13,11 @@ class ReleaseTypeSeeder extends Seeder
     public function run(): void
     {
         $rows = [
-            ['slug' => 'studio_albums', 'name' => 'Studio Albums'],
-            ['slug' => 'compilations', 'name' => 'Compilations'],
-            ['slug' => 'singles_and_eps', 'name' => 'Singles & EPs'],
-            ['slug' => 'videos', 'name' => 'Videos'],
-            ['slug' => 'others', 'name' => 'Others'],
+            ['name' => 'Studio Albums'],
+            ['name' => 'Compilations'],
+            ['name' => 'Singles & EPs'],
+            ['name' => 'Videos'],
+            ['name' => 'Others'],
         ];
 
         ReleaseType::truncate();
